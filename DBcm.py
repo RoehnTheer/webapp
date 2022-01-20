1c1294fa0c975eb5df510f3d00ffa24017291459
Mikhail Tikhonov <archaon3508@gmail.com>
2022-01-20 13:22:07 +0300

    third

--- DBcm.py
+++ DBcm.py
@@ -1,6 +1,15 @@
 import mariadb
 
 
+class ConnectionError(Exception):
+    pass
+
+class CredentialsError(Exception):
+    pass
+
+class SQLError(Exception):
+    pass
+
 class UseDatabase:
     def __init__(self, config: dict) -> None:
         #Передаём словарь с параметрами подключения
@@ -8,9 +17,15 @@ class UseDatabase:
 
     def __enter__(self) -> 'cursor':
         #Подключаемся к БД и создаём курсор
-        self.conn = mariadb.connect(**self.configuration)
-        self.cursor = self.conn.cursor()
-        return self.cursor
+        try:
+            self.conn = mariadb.connect(**self.configuration)
+            self.cursor = self.conn.cursor()
+            return self.cursor
+        except mariadb.InterfaceError as err:
+            raise ConnectionError(err)
+        except mariadb.ProgrammingError as err:
+            raise CredentialsError(err)
+
 
     def __exit__(self, exc_type, exc_value, exc_trace) -> None:
         #Приказываем БД быстро, решительно записать данные
@@ -18,3 +33,7 @@ class UseDatabase:
         #Закрываем курсор и соединение с БД
         self.cursor.close()
         self.conn.close()
+        if exc_type is mariadb.ProgrammingError:
+            raise SQLError(exc_value)
+        elif exc_type:
+            exc_type(exc_value)
--- search4web.py
+++ search4web.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, escape, session
+from threading import Thread
+from time import sleep
+from flask import Flask, render_template, request, escape, session, copy_current_request_context
 from vsearch import search4letters
 from checker import check_logged_in
-from DBcm import UseDatabase
+from DBcm import UseDatabase, ConnectionError, CredentialsError, SQLError
 
 
 app = Flask(__name__)
@@ -12,29 +14,54 @@ app.config['dbconfig'] = {'host': '127.0.0.1',
                           'password': '1488',
                           'database': 'vsearchlogDB',}
 
-def log_request(req: 'flask_request', res: str) -> None:
-
-    with UseDatabase(app.config['dbconfig']) as cursor:
-        #Пишем форму запроса
-        _SQL = """insert into log 
-                (phrase, letters, ip, browser_string, results)
-                values
-                (%s, %s, %s, %s, %s)"""
-        #Отправляем запрос к БД
-        cursor.execute(_SQL, (req.form['phrase'],
-                            req.form['letters'],
-                            req.remote_addr,
-                            req.user_agent.browser,
-                            res,))
+# def log_request(req: 'flask_request', res: str) -> None:
+#     """Сохраняет фразу, символы, результат, IP и используемый браузер в БД"""
+#     sleep(15)
+#     with UseDatabase(app.config['dbconfig']) as cursor:
+#         #Пишем форму запроса
+#         _SQL = """insert into log 
+#                 (phrase, letters, ip, browser_string, results)
+#                 values
+#                 (%s, %s, %s, %s, %s)"""
+#         #Отправляем запрос к БД
+#         cursor.execute(_SQL, (req.form['phrase'],
+#                             req.form['letters'],
+#                             req.remote_addr,
+#                             req.user_agent.browser,
+#                             res,))
 
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
+    """Принимает запрос, обрабатывает его, вызывает функцию сохранения в БД и выводит результат"""
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = "Result of your request: "
     results = str(search4letters(phrase, letters))
-    log_request(request, results)
+
+    @copy_current_request_context
+    def log_request(req: 'flask_request', res: str) -> None:
+        """Сохраняет фразу, символы, результат, IP и используемый браузер в БД"""
+        sleep(15)
+        with UseDatabase(app.config['dbconfig']) as cursor:
+            #Пишем форму запроса
+            _SQL = """insert into log 
+                    (phrase, letters, ip, browser_string, results)
+                    values
+                    (%s, %s, %s, %s, %s)"""
+            #Отправляем запрос к БД
+            cursor.execute(_SQL, (req.form['phrase'],
+                                req.form['letters'],
+                                req.remote_addr,
+                                req.user_agent.browser,
+                                res,))
+
+    try:
+        #log_request(request, results)
+        t = Thread(target=log_request, args=(request, results))
+        t.start()
+    except Exception as err:
+        print('****** Ошибка:', str(err))
     return render_template('results.html',
                             the_phrase=phrase,
                             the_letters=letters,
@@ -44,26 +71,37 @@ def do_search() -> 'html':
 @app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
+    """Стартовая страница с формой для запроса"""
     return render_template('entry.html', the_title='Welcome to the search4letters on the web!')
 
 @app.route('/viewlog')
 @check_logged_in
 def view_the_log() -> 'html':
+    """Вытягивает из БД историю запросов и позволяет посмотреть её залогиненным юзерам"""
     # contents = [] Старая версия для записи в лог-файл
     # with open('vsearch.log') as log:
     #     for line in log:
     #         contents.append([])
     #         for item in line.split('|'):
     #             contents[-1].append(escape(item))
-    with UseDatabase(app.config['dbconfig']) as cursor:
-        _SQL = """select phrase, letters, ip, browser_string, results from log"""
-        cursor.execute(_SQL) #Отправляем запрос к БД
-        contents = cursor.fetchall() #Извлекаем данные и присваиваем их переменной contents
-    titles = ('Phrase','Letters', 'Remote addr', 'User Agent', 'Results')
-    return render_template('viewlog.html',
-                            the_title='View log',
-                            the_row_titles=titles,
-                            the_data=contents,)
+    try:
+        with UseDatabase(app.config['dbconfig']) as cursor:
+            _SQL = """select phrase, letters, ip, browser_string, results from log"""
+            cursor.execute(_SQL) #Отправляем запрос к БД
+            contents = cursor.fetchall() #Извлекаем данные и присваиваем их переменной contents
+        titles = ('Phrase','Letters', 'Remote addr', 'User Agent', 'Results')
+        return render_template('viewlog.html',
+                                the_title='View log',
+                                the_row_titles=titles,
+                                the_data=contents,)
+    except ConnectionError as err:
+        print('****** Чё-то с базой данных:', str(err))
+    except CredentialsError as err:
+        print('****** Чё-то с именем пользователя или/и паролем:', str(err))
+    except SQLError as err:
+        print('****** Чё-то с запросом в БД:', str(err))
+    except Exception as err:
+        print('**** Что-то пошло не так ****:', str(err))
 
 @app.route('/login')
 def do_login() -> str:
